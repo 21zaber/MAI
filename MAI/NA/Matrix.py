10fb12c6959267d01bc2d370a7b6b3d1ddd7b247
21zaber <zaber.eng@gmail.com>
2016-06-06 22:57:28 +0300

    NA

--- MAI/NA/Matrix.py
+++ MAI/NA/Matrix.py
@@ -2,55 +2,86 @@ from copy import deepcopy as copy
 from Vector import Vector
 
 class Matrix(Vector):
-    #def __init__(self, m = Vector([Vector([1])])):
-    #    self.check_matrix()
+    @staticmethod
+    def new(n = 1, m = 1):
+        v = Vector.new(m)
+        return Matrix([copy(v) for i in range(n)])
 
-    def check_matrix(self):
-        if len(self) == 0:
-            return
-        n = len(self[0])
-        for i in self:
-            if len(i) != n:
-                raise TypeError
-
-    def set_size(self, n = 1, m = 1):
-        self = Matrix([Vector([0 for i in range(m)]) for i in range(n)])
-        for i in range(len(self)):
-            for j in range(len(self[i])):
-                a[i][j] = self[i][j]
-        print "a: ", a
-        self = a
-
+    @staticmethod
+    def E(n = 1):
+        m = Matrix.new(n, n)
+        for i in range(n):
+            m[i][i] = 1
+        return m
+    
     def __str__(self):
         s = ''
         for i in self:
-            s += str(i)
-        return s[:-1]
+            s += str(i) + '\n'
+        return s[:-1] 
 
-    def get_column(self, n):
-        return [self[i][n] for i in range(self.n)]
+    def get_col(self, n):
+        m = Matrix.new(len(self), 1)
+        for i in range(len(self)):
+            m[i][0] = self[i][n]
+        return m
+    def get_row(self, n):
+        return self[n]
+    def swap_col(self, a, b):
+        for i in range(len(self)):
+            self[i][a], self[i][b] = self[i][b], self[i][a]
+    def swap_row(self, a, b):
+        self[a], self[b] = self[b], self[a]
+    
+    def transpose(self):
+        m = Matrix.new(len(self[0]), len(self))
+        for i in range(len(self)):
+            for j in range(len(self[0])):
+                m[j][i] = self[i][j]
+        return m
+
+    def list(self):
+        if len(self) != 1 and len(self[0]) != 1:
+            raise TypeError
+        if len(self) == 1:
+            return self[0]
+        else:
+            return Vector([self[i][0] for i in range(len(self))])
 
     def __mul__(a, b):
         if type(b) == int or type(b) == float:
             c = copy(a)
-            for i in range(a.n):
-                for j in range(a.m):
+            for i in range(len(a)):
+                for j in range(len(a[0])):
                     c[i][j] *= b
             return c
         if type(b) == Matrix:
-            if a.m != b.n:
+            if len(a[0]) != len(b):
                 raise TypeError
-            c = Matrix().set_size(a.n, b.m)
-            for i in range(c.n):
-                for j in range(c.m):
+            c = Matrix.new(len(a), len(b[0]))
+            for i in range(len(c)):
+                for j in range(len(c[0])):
                     c[i][j] = 0
-                    for k in range(a.m):
+                    for k in range(len(a[0])):
                         c[i][j] += a[i][k] * b[k][j]
             return c
-        raise TypeError    
+        raise TypeError 
+    def __add__(a, b):
+        if type(b) != type(a):
+            raise TypeError
+        if len(a) != len(b):
+            raise TypeError
+        if len(a) == 0:
+            return copy(a)
+        if len(a[0]) != len(b[0]):
+            raise TypeError
+        if len(a[0]) == 0:
+            return copy(a)
+        c = Matrix.new(len(a), len(a[0]))
+        for i in range(len(a)):
+            for j in range(len(a[0])):
+                c[i][j] = a[i][j] + b[i][j]
+        return c
 
-t = Matrix([Vector([1])])
-m = t
-print m[0][0]
-m.set_size(2, 3)
-print m
+    def __sub__(a, b):
+        return a + (b * -1)
--- MAI/NA/Vector.py
+++ MAI/NA/Vector.py
@@ -1,6 +1,19 @@
 from copy import deepcopy as copy
 
+def f2s(a):
+    #return str(a)
+    a = round(a, 3)
+    if a <= 0 and abs(a) < 0.001:
+        a = abs(a)
+    if a >= 0:
+        return ' ' + str(a).ljust(5, '0')[:5]
+    else:
+        return str(a).ljust(6, '0')[:6]
+
 class Vector(list):
+    @staticmethod
+    def new(n = 1):
+        return Vector([0 for i in range(n)])
     def __mul__(a, b):
         if type(b) == int or type(b) == float:
             c = copy(a)
@@ -16,22 +29,23 @@ class Vector(list):
             for i in range(len(c)):
                 c[i] += b[i]
             return c
-
         raise TypeError
+
+    def __sub__(a, b):
+        if type(b) != type(a):
+            raise TypeError
+        return a + (b * -1)
     
     def __str__(self):
-        return ' '.join(list(map(lambda s: str(s).ljust(4), self)))
+        return ' '.join(list(map(f2s, self)))
 
     def __abs__(self):
         c = 0
         for i in self:
-            c += abs(i)
+            a = abs(i)
+            if a > c:
+                c = a
         return c
 
     def read(self, s):
         self = list(map(float, s.split()))
-
-v = Vector([1, 2, 3])
-
-print v
-print abs(v)
