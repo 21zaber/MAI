b2353e61fa4169c47989bc47eb1cd705d9960669
21zaber <zaber.eng@gmail.com>
2016-10-31 20:21:01 +0300

    reworked cursach

--- MAI/OScur/new/main.cpp
+++ MAI/OScur/new/main.cpp
@@ -1,9 +1,14 @@
 #include "stdlib.h"
 #include "stdint.h"
 #include "stdio.h"
-#include "string.h"
+#include "string.h"  
+#include <ctime>
 #include <iostream>
 #include <vector>
+
+#define SIMPLE 1
+
+//#define LOGS 1
 
 using namespace std;
 
@@ -15,6 +20,7 @@ typedef struct element_s {
 
 #define FOREACH(I, HEAD) for(I=HEAD;I!=NULL&&I->next!=I;I=I->next)
 #define FOREACH_SAFE(I, HEAD, TMP) for(I=HEAD,TMP=I->next;(I!=NULL)and(TMP=I->next);I=TMP)
+
 
 #define INSERT(NEW, ELEM)           \
 {                                   \
@@ -71,10 +77,13 @@ void init(size_t size) {
 void *alloc(size_t size) {
     element_t *elem = NULL, *nelem = NULL;
     element_t *res = NULL;
+#ifdef SIMPLE
     FOREACH(elem, head) {
         if (!elem->used and elem->size >= size) {
             size_t excess_mem = elem->size - size - sizeof(element_t); 
+#ifdef LOGS
             cout << "Allocation:   " << size << " bytes" << endl;
+#endif
             if (elem->size > size + sizeof(element_t)) {
                 res = SUM(elem, sizeof(element_t), element_t*);
                 nelem = SUM(res, size, element_t*); 
@@ -91,7 +100,42 @@ void *alloc(size_t size) {
             }
         }    
     }
+#else 
+    element_t *best = NULL;
+    FOREACH(elem, head) {
+        if (!elem->used and elem->size >= size) {
+            if (best == NULL or best->size > elem->size){
+                best = elem;
+                //cout << "1";
+            }    
+        }    
+    }
+
+    if (best != NULL) {
+        size_t excess_mem = best->size - size - sizeof(element_t); 
+#ifdef LOGS
+        cout << "Allocation:   " << size << " bytes" << endl;
+#endif
+        if (best->size > size + sizeof(element_t)) {
+            res = SUM(best, sizeof(element_t), element_t*);
+            nelem = SUM(res, size, element_t*); 
+            nelem->used = false;
+            nelem->size = excess_mem;
+            INSERT(nelem, best);
+            best->used = true;
+            best->size = size;
+            return res;
+        } else {
+            res = SUM(best, sizeof(element_t), element_t*);
+            best->used = true;
+            return res;
+        }
+    }
+#endif
+
+#ifdef LOGS
     cout << "Allocation:   " << size << " bytes. FAIL" << endl;
+#endif
     return NULL;
 }
 
@@ -108,7 +152,9 @@ void try_to_merge() {
 void dealloc(void *l) {
     element_t *elem = SUB(l, sizeof(element_t), element_t*);
     elem->used = false;
+#ifdef LOGS
     cout << "Deallocation: " << elem->size << " bytes" << endl;
+#endif
     MERGE_NEXT(elem);
     MERGE_NEXT(elem->prev);
 }
@@ -137,15 +183,19 @@ void print_mem_usage() {
     size_t prev = 0;
     size_t used_mem = 0;
     size_t sys_mem = 0;
+    size_t _mem = 0;
 
     FOREACH(elem, head) {
-        int n = elem->size * BUF_SIZE / allocated;
         int p = prev * BUF_SIZE / allocated;
         if (elem->used) {
-            memset(buf+p, '|', n);
+            _mem += elem->size + sizeof(element_t);
             used_mem += elem->size;
+        } else {
+            int n = _mem * BUF_SIZE / allocated;
+            memset(buf+p, '|', n);
+            prev += _mem + sizeof(element_t) + elem->size;
+            _mem = 0;
         }
-        prev += elem->size;
         used_mem += sizeof(element_t);
         sys_mem += sizeof(element_t);
     }
@@ -185,25 +235,23 @@ void test1() {
     destroy();
 }
 
-void test2() {
-    init(500 * 1000);
+void test2(size_t total_mem, uint32_t n, size_t max_block, size_t min_block) {
+    init(total_mem);
     vector<void*> elements;
-    for (int i = 0; i < 100; ++i) {
-        if ((rand() % 3) == 0 and elements.size() > 0) {
+    for (int i = 0; i < n; ++i) {
+        if ((rand() % 2) == 0 and elements.size() > 0) {
             int n = rand() % elements.size();
             dealloc(elements[n]);
-            vector<void*>::iterator iter = elements.begin();
-            for (int j = 0; j < n; iter++, j++){}
-            elements.erase(iter);
+            elements.erase(elements.begin() + n);
         } else {
-            size_t sz = rand() % 40000 + 100;
+            size_t sz = rand() % (max_block - min_block + 1) + min_block;
             void *new_elem = alloc(sz);
             if (new_elem != NULL) {
                 elements.push_back(new_elem);
                 memset(new_elem, 0, sz);
             }
         }
-        if (i % 10 == 9){
+        if (i % (int)(n / 10) == 0){
             //print_list();
             //printf("\n");
             print_mem_usage();
@@ -215,5 +263,7 @@ void test2() {
 
 
 int main() {
-    test2();
+    srand (time(NULL));
+    //test2(10*1000, 1000, 500, 500);
+    test2(10*1000, 1000, 1000, 100);
 }
